21c9d10af65cf52995ef8f4bf59a42cdd1788b76
Tissure <kyle.ng7512@gmail.com>
2023-02-21 14:44:52 -0800

    Implement basic task animation controller

--- AutoRepair-AR/Assets/Scripts/InstructionsUI.cs
+++ AutoRepair-AR/Assets/Scripts/InstructionsUI.cs
@@ -7,11 +7,27 @@ public class InstructionsUI : MonoBehaviour
 {
 
     public Button next, back, replay;
+    private int stepCounter = 0;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GameObject.Find("Model").GetComponent<Animator>();
+        next.onClick.AddListener(() => {
+            stepCounter++;
+            animator.SetInteger("StepCount", stepCounter);
+        });
+
+        back.onClick.AddListener(() => {
+            stepCounter--;
+            animator.SetInteger("StepCount", stepCounter);
+        });
+
+        replay.onClick.AddListener(() => {
+            Debug.Log("Replaying: " + animator.GetCurrentAnimatorStateInfo(0).fullPathHash);
+            animator.Play(animator.GetCurrentAnimatorStateInfo(0).fullPathHash, -1, 0);
+        });
     }
 
 }
