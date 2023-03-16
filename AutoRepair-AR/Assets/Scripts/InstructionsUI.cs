e2290a7649c0373aba7504359744ef80e0470fca
Kyle <kyle.ng7512@gmail.com>
2023-03-15 18:39:02 -0700

    Connect UI with controller

--- AutoRepair-AR/Assets/Scripts/InstructionsUI.cs
+++ AutoRepair-AR/Assets/Scripts/InstructionsUI.cs
@@ -2,37 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.XR.ARKit;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class InstructionsUI : MonoBehaviour
 {
 
-    public Button next, back, replay;
+    public Button next, back;
     private int stepCounter = 0;
     Task task;
 
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
+        // Get Task
         task = GameObject.FindGameObjectWithTag("Task").GetComponent<Task>();
+        // Get Buttons
+        getButtons();
+
         //animator = GameObject.Find("Model").GetComponent<Animator>();
-        next.onClick.AddListener(() => {
+        next.clicked += () => {
             //stepCounter++;
             //animator.SetInteger("StepCount", stepCounter);
             task.NextStep();
-        });
+        };
 
-        back.onClick.AddListener(() => {
+        back.clicked += () => {
             //if (stepCounter - 1 >= 0) stepCounter--;
             //animator.SetInteger("StepCount", stepCounter);
             task.PreviousStep();
-        });
+        };
 
-        replay.onClick.AddListener(() => {
-            //Debug.Log("Replaying: " + animator.GetCurrentAnimatorStateInfo(0).fullPathHash);
-            //animator.Play(animator.GetCurrentAnimatorStateInfo(0).fullPathHash, -1, 0);
-            task.ReplayStep();
-        });
+        //replay.onClick.AddListener(() => {
+        //    //Debug.Log("Replaying: " + animator.GetCurrentAnimatorStateInfo(0).fullPathHash);
+        //    //animator.Play(animator.GetCurrentAnimatorStateInfo(0).fullPathHash, -1, 0);
+        //    task.ReplayStep();
+        //});
+    }
+
+    void getButtons()
+    {
+        VisualElement root = GameObject.Find("UIDocument").GetComponent<UIDocument>().rootVisualElement;
+
+        next = root.Q<Button>("NextBtn");
+        back = root.Q<Button>("BackBtn");
+
     }
 
 }
