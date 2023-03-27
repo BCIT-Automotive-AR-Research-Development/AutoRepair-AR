0fe3c6b4d53f0b3b9a6b76739d5e00edbc2f3ac8
DESKTOP-Q225OBG\Jason <joh67@my.bcit.ca>
2023-03-26 22:03:10 -0700

    Opened new branch, added ui scripts to increase functionality

---
+++ AutoRepair-AR/Assets/Scripts/UIElementToggler.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIElementToggler : MonoBehaviour
+{
+    public VisualElement leftBtn;
+    public VisualElement rightBtn;
+    public VisualElement info;
+    public VisualElement infoText;
+
+    private void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        Button stepUpBtn = root.Q<Button>("StepUpBtn");
+        Button stepDownBtn = root.Q<Button>("StepDownBtn");
+
+        leftBtn = root.Q<VisualElement>("LeftBtn");
+        rightBtn = root.Q<VisualElement>("RightBtn");
+        info = root.Q<VisualElement>("Info");
+        infoText = root.Q<VisualElement>("InfoText");
+
+        // Hide the info text and disable the step down button on start
+        infoText.style.display = DisplayStyle.None;
+        stepDownBtn.SetEnabled(false);
+
+        stepUpBtn.clickable.clicked += () =>
+        {
+            // Hide the left button, right button, and info element
+            leftBtn.style.display = DisplayStyle.None;
+            rightBtn.style.display = DisplayStyle.None;
+            info.style.display = DisplayStyle.None;
+            stepDownBtn.style.display = DisplayStyle.Flex;
+
+            // Show the info text element
+            infoText.style.display = DisplayStyle.Flex;
+            stepUpBtn.style.display = DisplayStyle.None;
+
+            // Enable the step down button
+            stepDownBtn.SetEnabled(true);
+        };
+
+        stepDownBtn.clickable.clicked += () =>
+        {
+            // Show the left button, right button, and info element
+            leftBtn.style.display = DisplayStyle.Flex;
+            rightBtn.style.display = DisplayStyle.Flex;
+            info.style.display = DisplayStyle.Flex;
+            stepUpBtn.style.display = DisplayStyle.Flex;
+
+            // Hide the info text element
+            infoText.style.display = DisplayStyle.None;
+            stepDownBtn.style.display = DisplayStyle.None;
+
+            // Disable the step down button
+            stepDownBtn.SetEnabled(false);
+        };
+    }
+}
