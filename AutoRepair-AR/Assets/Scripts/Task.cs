260379a45c63e202c923e273700ad84f5a41f59f
JungMin Oh <joh67@my.bcit.ca>
2023-04-26 14:05:46 -0700

    UI bugs fixed - scroll slider functionality & instruction descriptions displaying properly.

--- AutoRepair-AR/Assets/Scripts/Task.cs
+++ AutoRepair-AR/Assets/Scripts/Task.cs
@@ -44,6 +44,28 @@ public class Task : MonoBehaviour
         }
     }
 
+    public void GoToStep(int stepToGo){
+        stepCounter = stepToGo;
+        //animator.clip = instructions[stepCounter].clip;
+        //animator.Play();
+        animatorOverrideController["Initial"] = instructions[stepCounter - 1].clip;
+        animator.Play("Initial", -1, 0);
+    }
+
+    public List<Step> getInstructions(){
+        return instructions;
+    }
+
+    public string[] GetStepDescriptions(){
+        string[] descriptions = new string[instructions.Count + 1];
+        for (int i = 0; i < instructions.Count; i++){
+            descriptions[i] = instructions[i].Desc;
+        }
+        descriptions[instructions.Count] = "All Done!";
+    return descriptions;
+    }
+
+
     public void ReplayStep()
     {
         animator.Play("Step", -1, 0);
